c6daa1cb6563631739d7a170f7d420e25d2df7ad
Armando Buruain <armandoburuain@Armandos-MacBook-Air.local>
2024-12-03 14:30:31 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>Welcome to the Front-End Developer Test!</h1>
+      <h1>Welcome to the Front-End Developer Test! for Armando</h1>
       <p>We wish you the best of luck. Please make sure to read the README file for
         instructions.</p>
     </div>
