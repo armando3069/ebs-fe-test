576fe37d39980db544747b3fab4f10078496a5ff
Armando Buruain <armandoburuain@Armandos-MacBook-Air.local>
2024-12-05 10:21:17 +0200

    refactor: remove footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/home/home';
-import Shop from './pages/shop/shop';
-import {CartProvider } from './context/shopContext';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/home";
+import Shop from "./pages/shop/shop";
+import Navbar from "./components/navbar/navbar";
+import { CartProvider } from "./context/shopContext";
+import Footer from "./components/footer/footer";
 const App: React.FC = () => {
   return (
     <CartProvider>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Shop />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Shop />} />
+        </Routes>
+      </Router>
     </CartProvider>
   );
 };
