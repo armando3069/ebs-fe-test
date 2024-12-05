277b5841b0351be1ecffcdda10f20bf50674f00e
Armando Buruain <armandoburuain@Armandos-MacBook-Air.local>
2024-12-05 11:00:08 +0200

    fix: restyle code

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ const App: React.FC = () => {
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Shop />} />
         </Routes>
+        <Footer />
       </Router>
     </CartProvider>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Footer from "./components/footer/footer";
+
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <div className="main-content">
         <App />
-      </div>    
-      <Footer />
+
   </StrictMode>,
 )
