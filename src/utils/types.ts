597e9eb553c6466697db0b5d4e6208d1d4892b3f
Armando Buruain <armandoburuain@Armandos-MacBook-Air.local>
2024-12-05 10:41:09 +0200

    perf: add  types and interface

---
+++ src/utils/types.ts
@@ -0,0 +1,33 @@
+import { ReactNode } from "react";
+
+export type CartItem = {
+  id: number;
+  quantity: number;
+};
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
+
+export type CartProviderProps = {
+  children: ReactNode;
+};
+
+export type CartContext = {
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+  cartQuantity: number;
+  cartItems: CartItem[];
+  products: Product[];
+  clearCart: () => void;
+};
