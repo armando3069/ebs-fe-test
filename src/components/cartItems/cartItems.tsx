9ade07f29360dfd6f50ade0b7284af190726b814
Armando Buruain <armandoburuain@Armandos-MacBook-Air.local>
2024-12-05 10:24:20 +0200

    refactor: change style code

---
+++ src/components/cartItems/cartItems.tsx
@@ -0,0 +1,60 @@
+import { useShoppingCart } from "../../context/shopContext";
+import { HiOutlineX } from "react-icons/hi";
+
+import "./index.css";
+
+type CartItemProps = {
+  id: number;
+  quantity: number;
+};
+
+const CartItem = ({ id, quantity }: CartItemProps) => {
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+    products,
+  } = useShoppingCart();
+
+  const product = products.find((product) => product.id === id);
+
+  if (!product) return null; // Returnează nimic dacă produsul nu este găsit
+
+  return (
+    <div className="cart-item">
+      <div className="cart-item-product">
+        <img
+          src={product.image}
+          alt={product.title}
+          className="cart-item-image"
+        />
+        <h5>{product.title}</h5>
+      </div>
+
+      <div className="cart-item-details">
+        <p className="cart-item-price">${product.price}</p>
+
+        <div className="cart-item-actions">
+          <button className="btn-cart" onClick={() => decreaseCartQuantity(id)}>
+            -
+          </button>
+          <p>{quantity}</p>
+          <button
+            className="btn-cart"
+            id="mb"
+            onClick={() => increaseCartQuantity(id)}
+          >
+            +
+          </button>
+        </div>
+        <p>${(product.price * quantity).toFixed(2)}</p>
+        <button className="btn-cart" onClick={() => removeFromCart(id)}>
+          <HiOutlineX size={20} color="grey" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
