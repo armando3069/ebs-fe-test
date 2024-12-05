faf8e69afa00ced4e1c10d104b0817abbc8cc571
Armando Buruain <armandoburuain@Armandos-MacBook-Air.local>
2024-12-05 10:20:32 +0200

    refactor: add a class 🌝

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import Footer from "./components/footer/footer";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <div className="main-content">
+        <App />
+      </div>    
+      <Footer />
   </StrictMode>,
 )
